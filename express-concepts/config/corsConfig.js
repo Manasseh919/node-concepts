0373d756f8cce5076fbf302952e4e98bd07f3f10
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-02-21 16:16:54 +0100

    cors

--- express-concepts/config/corsConfig.js
+++ express-concepts/config/corsConfig.js
@@ -1,15 +1,27 @@
-const cors = require('cors')
+const cors = require("cors");
 
+const configureCors = () => {
+  return cors({
+    //first we return the origin - this will tell which origins you want it to be accessed by users  in the api
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000", //local dev
+        "https://yourcustondomain.com", //production domain
+      ];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true); //giving permission so that request can be allowed
+      } else {
+        callback(new Error("Not allowed by cors"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept-Version"],
+    exposedHeaders: ["X-Total-Count", "Content-Range"],
+    credentials: true, //enable support for cookies
+    preflightContinue: false,
+    maxAge: 600, //cache pre flight responses for 10 minutes (600seconds) -> avoid sending options requests multiple times
+    optionsSuccessStatus: 204,
+  });
+};
 
-const configureCors = () =>{
-    return cors({
-        //first we return the origin - this will tell which origins you want it to be accessed by users  in the api
-        origin:(origin,callback)=>{
-            const allowedOrigins = [
-                'http://localhost:3000',
-                
-            ]
-        }
-
-    })
-}
+module.exports = {configureCors}
--- express-concepts/server.js
+++ express-concepts/server.js
@@ -1,17 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
+const { configureCors } = require("./config/corsConfig.js");
 
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 //express json middleware
-app.use(express.json())
+app.use(configureCors());
+app.use(express.json());
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
