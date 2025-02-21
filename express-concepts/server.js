fba04cafa64cbcc196940370e04bba83ba9194a0
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-02-21 16:24:28 +0100

    custom middleware

--- express-concepts/server.js
+++ express-concepts/server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const { configureCors } = require("./config/corsConfig.js");
 
+
 const express = require("express");
 const cors = require("cors");
+const { requestLogger, addTimeStamp } = require("./middleware/customMiddleware.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //express json middleware
+app.use(requestLogger)
+app.use(addTimeStamp)
+
 app.use(configureCors());
 app.use(express.json());
 
