acbc6a798dd821c3a34fe6ac10f0482476838cf6
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-02-21 16:48:42 +0100

    api versioning

--- express-concepts/server.js
+++ express-concepts/server.js
@@ -1,3 +1,5 @@
+//-> major releases -> API -> v1,v2
+
 require("dotenv").config();
 const { configureCors } = require("./config/corsConfig.js");
 
@@ -8,6 +10,7 @@ const {
   addTimeStamp,
 } = require("./middleware/customMiddleware.js");
 const { globalErrorHandler } = require("./middleware/errorHandler.js");
+const { urlVersioning } = require("./middleware/apiVersioning.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +21,8 @@ app.use(addTimeStamp);
 
 app.use(configureCors());
 app.use(express.json());
+
+app.use("/api/v1", urlVersioning("v1"));
 
 app.use(globalErrorHandler);
 
