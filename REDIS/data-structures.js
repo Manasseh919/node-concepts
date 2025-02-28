a106e8790cd5ce55552a1769d1803b8f23d1ce2e
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-02-28 09:03:47 +0000

    Sets redis

--- REDIS/data-structures.js
+++ REDIS/data-structures.js
@@ -19,7 +19,7 @@ const redisDataStructures = async () => {
     //strings -> SET,GET,MSET,MGET
     await client.set("user:name", "Software Dev");
     const name = await client.get("user:name");
-    console.log(name);
+    // console.log(name);
 
     await client.mSet([
       "user:email",
@@ -34,19 +34,33 @@ const redisDataStructures = async () => {
       "user:age",
       "user:country",
     ]);
-    console.log(email, age, country);
+    // console.log(email, age, country);
 
     //lists => LPUSH,RPUSH,LRANGE,LPOP,RPOP
 
-    await client.lPush("notes", ["note 1", "note 2", "note 3"]);
-    const extractAllNotes = await client.lRange("notes", 0, -1);
-    console.log(extractAllNotes);
+    // await client.lPush("notes", ["note 1", "note 2", "note 3"]);
+    // const extractAllNotes = await client.lRange("notes", 0, -1);
+    // console.log(extractAllNotes);
 
-    const firstNOte = await client.lPop("notes");
-    console.log(firstNOte);
+    // const firstNOte = await client.lPop("notes");
+    // console.log(firstNOte);
 
-    const remainingNOtes = await client.lRange("notes", 0, -1);
-    console.log(remainingNOtes)
+    // const remainingNOtes = await client.lRange("notes", 0, -1);
+    // console.log(remainingNOtes)
+
+
+    //sets -> SADD,SMEMBERS,SISMEMBER,SREM
+
+    await client.sAdd('user:nickName',['john','varun','xyz'])
+    const extractUserUniquenames = await client.sMembers('user:nickName')
+    console.log(extractUserUniquenames)
+
+    const isVarunOneOfUserNickName = await client.sIsMember('user:nickName','john')
+    console.log(isVarunOneOfUserNickName)
+
+    await client.sRem('user:nickName','xyz')
+    const getUPdatedUserNickNames = await client.sMembers("user:nickName")
+    console.log(getUPdatedUserNickNames)
   } catch (error) {
     console.log(error);
   } finally {
