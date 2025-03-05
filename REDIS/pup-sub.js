713f8919df7c385e30f23a87939430af99c8edf9
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-05 01:57:03 +0000

    pipelining

--- REDIS/pup-sub.js
+++ REDIS/pup-sub.js
@@ -36,6 +36,70 @@ async function testAdditionalFeatures() {
     await subscriber.unsubscribe("dummy-channel");
 
     await subscriber.quit();
+
+    //pipelining & transactions
+    const multi = client.multi();
+
+    // multi.set("key-transaction1", "value1");
+    // multi.set("key-transaction2", "value2");
+
+    // multi.get("key-transaction1");
+    // multi.get("key-transaction2");
+
+    // const results = await multi.exec();
+    // console.log(results);
+
+    // const pipeline = client.multi();
+    // multi.set("key-pipeline1", "value1");
+    // multi.set("key-pipeline2", "value2");
+
+    // multi.get("key-pipeline1");
+    // multi.get("key-pipeline2");
+
+    // const pipelineresults = await multi.exec();
+    // console.log(pipelineresults);
+
+    // //batch data operations
+    // const pipeLineOne = client.multi();
+    // for (let i = 0; i < 1000; i++) {
+    //   pipeline.set(`user:${i}:action`, `Action ${i}`);
+    // }
+
+    // await pipeLineOne.exec();
+
+    // const dummyExample = client.multi();
+    // multi.decrBy('account:1234:balance',100)
+    // multi.incrBy('account:0000:balance',100)
+
+    // const finalresults = await multi.exec()
+
+    // console.log(finalresults)
+
+    // const cartExample = client.multi();
+    // multi.hIncrBy('cart:1234',"product-1",1)
+
+    console.log('Performance Test')
+    console.log("without pipelining")
+
+    for(let i= 0;i<1000;i++){
+      await client.set(`user:${i}`, `user_values ${i}`);
+    }
+
+    console.timeEnd("without pipelining")
+
+    console.time("with pipelining")
+
+
+    const bigPipeline = client.multi();
+
+    for(let i = 0;i<1000;i++){
+      bigPipeline.set(`user_pipeline_key${i}`, `user_pipeline_value ${i}`);
+    }
+
+    await bigPipeline.exec();
+
+    console.timeEnd("with pipelining")
+
   } catch (error) {
     console.log(error);
   } finally {
@@ -43,5 +107,4 @@ async function testAdditionalFeatures() {
   }
 }
 
-
-testAdditionalFeatures()
+testAdditionalFeatures();
